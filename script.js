5302c3e095a0288e313a595109d74ee2a14c9d4f
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-05-10 13:53:08 -0300

    feat: estruturação e estilização do 1° layout

--- script.js
+++ script.js
@@ -11,19 +11,31 @@ const obj = [
         sentido: 'X',
         horario: 11.17
     },
+    {
+        sentido: 'X',
+        horario: 11.30
+    }
 ]
 
 const data = new Date();
 const horas = data.getHours();
 const minutos = data.getMinutes();
-const horasMinutos = [horas, minutos]. join('.');
+const horasMinutos = [horas, minutos].join('.');
 const horasMinutosFormatados = parseFloat(horasMinutos)
 
 obj.forEach(index => {
     const diferenca = index.horario - horasMinutosFormatados;
     const diferencaFormatado = parseFloat(diferenca.toFixed(2))
-    console.log(diferencaFormatado)
-    if(diferencaFormatado == 0.5 || diferencaFormatado < 0.5){
-        alert('deu certo!')
-    }
+    // if(diferencaFormatado > index.horario && diferencaFormatado <= 15){
+    //     alert ('passou')
+    // } else {
+    //     return false
+    // }
 })
+
+// const diferenca = index.horario - horasMinutosFormatados;
+//     const diferencaFormatado = parseFloat(diferenca.toFixed(2))
+//     console.log(diferencaFormatado)
+//     if(diferencaFormatado == 0.5 || diferencaFormatado < 0.5){
+//         alert('deu certo!')
+//     }
