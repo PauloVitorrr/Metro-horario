d45bee3bd681f9015a577dde1a46634bebeab6b9
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-05-11 14:08:19 -0300

    fix: ordenando array

--- script.js
+++ script.js
@@ -13,23 +13,31 @@ const obj = [
     },
     {
         sentido: 'X',
-        horario: 14.10
-    }
+        horario: 12.10 
+    },
+    {
+        sentido: 'X',
+        horario: 13.41
+    },
+    {
+        sentido: 'X',
+        horario: 13.45
+    },
 ]
 
 const data = new Date();
 const horas = data.getHours();
 const minutos = data.getMinutes();
 const horasMinutos = [horas, minutos].join(':');
-const horasMinutosFormatados = parseFloat(horasMinutos)
-
+const horasMinutosFormatados = parseFloat(horasMinutos.replace(':' , '.'))
+let proxValor = Infinity;
 
 document.getElementById("horario").innerHTML = horasMinutos;
 
-const max = obj.sort((a,b) => a.horario - b.horario)
-                .find(item => item.horario > horasMinutosFormatados);
-
-const y = [max.horario].toString().replace("." , ":")
-
-document.getElementById("prox-metro").innerHTML = y;
-console.log(max)
+for(let i = 0; i < obj.length; i++){
+     if(obj[i].horario > horasMinutosFormatados  && obj[i].horario < proxValor){
+        const horarioString = obj[i].horario.toString().replace('.', ':')
+        document.getElementById("prox-metro").innerHTML = horarioString;
+        proxValor = obj[i].horario;
+    }
+}
