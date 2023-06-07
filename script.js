f0f4e7c769a02585f7f703275d0990167d08bac9
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-06-07 11:10:39 -0300

    fix: ajuste do 0 antes do minuto

--- script.js
+++ script.js
@@ -1,43 +1,61 @@
 const obj = [
     {
-        sentido: 'X',
-        horario: 10.45
+        sentido: 'Chico da Silva',
+        horario: "11.01"
     },
     {
-        sentido: 'X',
-        horario: 11.01
+        sentido: 'Chico da Silva',
+        horario: "11.17"
     },
     {
-        sentido: 'X',
-        horario: 11.15
+        sentido: 'Chico da Silva',
+        horario: "11.33"
     },
     {
-        sentido: 'X',
-        horario: 12.10 
+        sentido: 'Chico da Silva',
+        horario: "12.10" 
     },
     {
-        sentido: 'X',
-        horario: 13.41
+        sentido: 'Chico da Silva',
+        horario: "13.41"
     },
     {
-        sentido: 'X',
-        horario: 13.45
+        sentido: 'Chico da Silva',
+        horario: "13.45"
     },
 ]
 
-const data = new Date();
-const horas = data.getHours();
-const minutos = data.getMinutes();
-const horasMinutos = [horas, minutos].join(':');
-const horasMinutosFormatados = parseFloat(horasMinutos.replace(':' , '.'))
-let proxValor = Infinity;
+function horarioAtual(){
+    const data = new Date();
+    const horas = data.getHours();
+    const minutos = data.getMinutes();
+    
+    const horasMinutos = `${horas}:${minutos.toString().padStart(2, '0')}`;
+    const horasMinutosFormatados = parseFloat(horasMinutos.replace(':', '.'));
+    
+    let proxValor = Infinity;
 
-document.getElementById("horario").innerHTML = horasMinutos;
+    document.getElementById("horario").innerHTML = horasMinutos;
 
-for(let i = 0; i < obj.length; i++){
-     if(obj[i].horario > horasMinutosFormatados  && obj[i].horario < proxValor){
-        const horarioString = obj[i].horario.toString().replace('.', ':')
-        document.getElementById("prox-metro").innerHTML = horarioString;
-        proxValor = obj[i].horario;
+    for(let i = 0; i < obj.length; i++){
+        if(obj[i].horario > horasMinutosFormatados && obj[i].horario < proxValor){
+            const horarioString = obj[i].horario.toString().replace('.', ':')
+            document.getElementById("prox-metro").innerHTML = horarioString;
+            proxValor = obj[i].horario;
+    }    
+}
+    const header = document.querySelector('.container-header')
+    const calc2 = proxValor - horasMinutosFormatados
+    const calc = parseFloat(calc2.toFixed(2))
+    if(calc <= 0.15 && calc > 0.10){
+        header.classList.add('red')
+    }
+    if(calc <= 0.10 && calc >= 0.03){
+        header.classList.add('yellow')
+    }
+    if(calc <= 0.03 && calc >= 0.01){
+        header.classList.add('green')
     }
 }
+
+setInterval(horarioAtual,1000)
